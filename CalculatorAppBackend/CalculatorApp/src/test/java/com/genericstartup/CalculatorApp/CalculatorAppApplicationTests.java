7d1450ef41d088bd2b4382350b0042be7686d0ea
Joshua Hilbert <hilbertjosh1@gmail.com>
2022-02-15 18:42:58 -0500

    Add spring backend directory

---
+++ CalculatorAppBackend/CalculatorApp/src/test/java/com/genericstartup/CalculatorApp/CalculatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genericstartup.CalculatorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
