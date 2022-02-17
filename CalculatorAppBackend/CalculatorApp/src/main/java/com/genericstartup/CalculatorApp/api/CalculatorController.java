4d7ef3960004d1a73a25736ed563d55fcfa13adf
Joshua Hilbert <hilbertjosh1@gmail.com>
2022-02-17 18:16:38 -0500

    Add calculator backend endpoint

---
+++ CalculatorAppBackend/CalculatorApp/src/main/java/com/genericstartup/CalculatorApp/api/CalculatorController.java
@@ -0,0 +1,22 @@
+package com.genericstartup.CalculatorApp.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+@RequestMapping("api/calculator")
+@RestController
+public class CalculatorController {
+
+    @GetMapping(path = "calculate/{expression}")
+    public String getCalculationResult(@PathVariable("string") String expression) throws ScriptException {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        return String.valueOf(engine.eval(expression));
+    }
+}
