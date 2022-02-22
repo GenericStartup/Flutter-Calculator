7741fbb4d6bdb57a976fade90e601f00f00c1780
ThorGuy <thor@thepages.net>
2022-02-21 21:03:56 -0500

    Added more comments to CalculatorButton
    Comments are more descriptive for presentation purposes

--- src/flutter_calculator/lib/CalculatorButton.dart
+++ src/flutter_calculator/lib/CalculatorButton.dart
@@ -5,36 +5,33 @@ import 'package:flutter/material.dart';
 // creating Stateless Widget for buttons, as even though interacting with them changes things, the buttons themselves do not change
 class CalculatorButton extends StatelessWidget {
   // declaring variables
-  final color;
-  final textColor;
-  final String buttonText;
-  final buttontapped;
-
+  final color; //Color
+  final textColor; //Color
+  final String buttonText; //String
+  final buttontapped; //Function
   //Constructor
-  CalculatorButton(
-      {this.color,
+  CalculatorButton({
+      this.color,
       this.textColor,
       required this.buttonText,
-      this.buttontapped}
-  );
-
+      this.buttontapped
+  });
   @override
   Widget build(BuildContext context) {
     return GestureDetector( // Widget that can detect actions involving it (touch, drag, etc.)
-      onTap: buttontapped,
+      onTap: buttontapped, // Function that responds to specified action (a tap or click in this case)
       child: Padding( // Puts a gap between buttons
-        padding: const EdgeInsets.all(0.2),
+        padding: const EdgeInsets.all(0.2), // Specifies the gap size to be 0.2 (Unsure about the unit)
         child: ClipRRect( // Constraints its contents to a rectangular area
-          // borderRadius: BorderRadius.circular(25),
           child: Container( // Actual widget that becomes visible
-            color: color,
+            color: color, // Color of the button
             child: Center( // Used to center text on button
-              child: Text( // Text itself
-                buttonText,
+              child: Text( // Widget for the text
+                buttonText, // Text itself
                 style: TextStyle( // Style of the text (color, font, size, etc.)
-                  color: textColor,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+                  color: textColor, // Color of the button
+                  fontSize: 25, // Size of the font
+                  fontWeight: FontWeight.bold, // Makes text bold
                 ),
               ),
             ),
